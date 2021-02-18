f6da66bfc86d0e160759c21d8854f86f038de862
Otso Velhonoja <48589417+veot@users.noreply.github.com>
2021-02-18 11:27:33 +0200

    3.9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const PORT = 3001;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
