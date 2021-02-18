5478a0e6be56ece8476575d8de0205fd28682991
Otso Velhonoja <48589417+veot@users.noreply.github.com>
2021-02-18 11:30:23 +0200

    3.10

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
