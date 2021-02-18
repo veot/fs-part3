6553f87fc4918f5887b248ca1ddf8a573117e8bb
Otso Velhonoja <48589417+veot@users.noreply.github.com>
2021-02-18 11:59:45 +0200

    uibuild

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
